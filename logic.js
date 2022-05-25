cd2192dc01452f16ff1d512c86b8d2eac9ff5eef
Jason McNeill <jmcneill@lesl.com>
2022-05-25 13:43:46 -0700

    Refactor

--- logic.js
+++ logic.js
@@ -1,24 +1,26 @@
 import isValidEmail from "./js/esm/isValidEmail.js";
 import watchInvalidEmail from "./js/esm/watchInvalidEmail.js";
 
-function validate(e) {
+function validateForm(e) {
   const emailEl = e.target.email;
   const feedbackEl = emailEl.parentElement.querySelector(".invalid-feedback");
   const email = emailEl.value.trim().toLowerCase();
-  let isValid = true;
+  const isValidEmailInput = isValidEmail(email);
+  let isValidForm = true;
 
-  if (!isValidEmail(email)) {
+  if (!isValidEmailInput) {
     feedbackEl.innerText = "A valid e-mail is required.";
     emailEl.classList.add("is-invalid");
     watchInvalidEmail();
+    isValidForm = false;
     return false;
   }
 
-  return isValid;
+  return isValidForm;
 }
 
 function onSubmit(e) {
-  const isValidForm = validate(e);
+  const isValidForm = validateForm(e);
 
   if (!isValidForm) {
     e.preventDefault();
