751a48d2e0a7cb31a9b85c7bde26961fb5ace6fb
Jason McNeill <jmcneill@lesl.com>
2022-05-26 09:18:57 -0700

    Enable linking

--- js/esm/getPhrases.js
+++ js/esm/getPhrases.js
@@ -10,9 +10,21 @@ export default function getPhrases(pathFromRoot, lang = "en") {
 
         if ((hasChanges) && Array.isArray(item.changes)) {
           item.changes.forEach((change) => {
-            if (change.bold) content = content.replaceAll(change.translated, `<strong>${change.translated}</strong>`);
-            if (change.italic) content = content.replaceAll(change.translated, `<em>${change.translated}</em>`);
-            if (change.underline) content = content.replaceAll(change.translated, `<u>${change.translated}</u>`);
+            const changeText = change.translated.trim();
+            let changeHTML = changeText;
+            if (!changeText.length) return;
+            if (change.link.href && change.link.href.length) {
+              const link = change.link;
+              changeHTML = `<a href='${link.href}'`;
+              if (link.rel && link.rel.length) changeHTML += ` rel='${link.rel}'`;
+              if (link.id && link.id.length) changeHTML += ` id='${link.id}'`;
+              if (link.class && link.class.length) changeHTML += ` class='${link.class}'`;
+              changeHTML += `>${changeText}</a>`;
+            }
+            if (change.underline) changeHTML = `<u>${changeHTML}</u>`;
+            if (change.italic) changeHTML = `<em>${changeHTML}</em>`;
+            if (change.bold) changeHTML = `<strong>${changeHTML}</strong>`;
+            content = content.replaceAll(changeText, changeHTML);
           });
         }
 
@@ -28,7 +40,7 @@ export default function getPhrases(pathFromRoot, lang = "en") {
         document.querySelectorAll(`[data-i18n='${item.id}']`).forEach((el) => {
           const phraseExists = item.phrase.trim().length ? true : false;
           if (phraseExists) {
-            el.innerText = item.phrase;
+            el.innerHTML = item.phrase;
           }
         });
       });
