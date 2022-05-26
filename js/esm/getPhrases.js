dc6824504a5e7f9175cc32d3b297047f5ce1bd82
Jason McNeill <jmcneill@lesl.com>
2022-05-25 18:48:52 -0700

    Remove link JSON cannot reliably contain HTML

--- js/esm/getPhrases.js
+++ js/esm/getPhrases.js
@@ -13,7 +13,6 @@ export default function getPhrases(pathFromRoot, lang = "en") {
             if (change.bold) content = content.replaceAll(change.translated, `<strong>${change.translated}</strong>`);
             if (change.italic) content = content.replaceAll(change.translated, `<em>${change.translated}</em>`);
             if (change.underline) content = content.replaceAll(change.translated, `<u>${change.translated}</u>`);
-            if (change.link.href.length) content = content.replaceAll(change.translated, `<a href="${change.href.link}" ${change.link.href.startsWith("http://") || change.link.startsWith("https://") ? "target='_blank' rel='noreferrer noopener nofollow'" : ""} ${change.link.id.length ? "id='" + change.link.id + "'" : ""} ${change.link.class.length ? "class='" + change.link.class + "'" : ""}>${change.translated}</a>`);
           });
         }
 
@@ -24,6 +23,8 @@ export default function getPhrases(pathFromRoot, lang = "en") {
 
         return phrase;
       });
+
+      console.log(phrases);
 
       phrases.forEach(item => {
         document.querySelectorAll(`[data-i18n='${item.id}']`).forEach((el) => {
