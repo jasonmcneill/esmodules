8a488938bcd23182a62806970beda1571d273691
Jason McNeill <jmcneill@lesl.com>
2022-05-31 09:46:35 -0700

    Add coordinates

--- logic.js
+++ logic.js
@@ -2,6 +2,12 @@ import isValidEmail from "./js/esm/isValidEmail.js";
 import watchInvalidEmail from "./js/esm/watchInvalidEmail.js";
 import getPhrases from "./js/esm/getPhrases.js";
 
+function showDetectLocation() {
+  if (navigator.geolocation) {
+    document.querySelector("#detectcoordinates_container").removeAttribute("hidden");
+  }
+}
+
 function validateForm(e) {
   const emailEl = e.target.email;
   const feedbackEl = emailEl.parentElement.querySelector(".invalid-feedback");
@@ -22,6 +28,35 @@ function validateForm(e) {
   return isValidForm;
 }
 
+function onDetectLocation() {
+  const coordinates = document.querySelector("#coordinates");
+  const lat = document.querySelector("#latitude");
+  const long = document.querySelector("#longitude");
+
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+
+  const success = (pos) => {
+    const coords = pos.coords;
+
+    lat.value = coords.latitude;
+    long.value = coords.longitude;
+    coordinates.scrollIntoView({ behavior: "smooth" });
+  }
+
+  const error = (err) => {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+
+    lat.value = "";
+    long.value = "";
+  }
+
+  navigator.geolocation.getCurrentPosition(success, error, options);
+}
+
 function onSubmit(e) {
   const isValidForm = validateForm(e);
 
@@ -32,10 +67,12 @@ function onSubmit(e) {
 
 function attachListeners() {
   document.querySelector("#myForm").addEventListener("submit", onSubmit);
+  document.querySelector("#detectcoordinates").addEventListener("click", onDetectLocation);
 }
 
 async function init() {
   pagePhrases = await getPhrases();
+  showDetectLocation();
   attachListeners();
 }
 
